c45ec9e125a99704c627714ded3504f24d526bd4
andrewkho <andrewkh@ece.ubc.ca>
2013-10-09 18:12:08 -0700

    Added intersection contour generation code. Should be Order N complexity where N is number of contour points

--- intersector.py
+++ intersector.py
@@ -18,6 +18,7 @@ class Intersector (object):
         mesh1, mesh2 must be of type QEMesh.
         tree1, tree2 must be of type AABBTree.
         """
+        self.clear_saved_results()
 
         if type(mesh1) is not quad_edge_mesh.QEMesh:
             raise TypeError("mesh1 must be of type QEMesh!")
@@ -28,13 +29,91 @@ class Intersector (object):
         if type(tree2) is not aabb_tree.AABBTree:
             raise TypeError("tree2 must be of type AABBTree!")
 
-
         ix_points = []
         pairs = tree1.collides_with_tree(tree2)
 
         for pair in pairs:
             new_ixpoints = self._intersect_faces(pair[0], pair[1])
             ix_points.extend(new_ixpoints)
+
+        ix_contours = self._create_intersection_contours(ix_points)
+
+        return ix_contours
+
+    def _create_intersection_contours(self, ix_points):
+        """ Return a list of intersection contours.
+        Each contour is an ordered list of IntersectionPoints.
+        """
+        if len(ix_points) is 0:
+            return []
+
+        contours = []
+        while len(ix_points) != 0:
+            contour = self._get_one_contour(ix_points)
+            contours.append(contour)
+            
+    def _get_one_contour(self, ix_points):
+        """ From a list of IntersectionPoints, construct a contour
+        by walking around. Removes IntersectionPoints from ix_points.
+        Returns a list of IntersectionPoints representing the contour.
+
+        A closed contour's first and last points are the same.
+        """
+        if len(ix_points) == 0:
+            return None
+
+        # A break condition if contour isn't closed
+        contour_is_open = False
+        first_ixp = ixp = ix_points[0]
+        next_ixp = None
+        contour = list(ixp)
+        prev_face = ixp.r_face
+        while next_ixp is not contour[0]:
+            next_ixp = None
+            # First search this edge's-face's edges for intersection with ixp.face
+            if ixp.edge.r_face is prev_face:
+                this_edges_face = ixp.edge.l_face
+            else:
+                this_edges_face = ixp.edge.r_face
+
+            if this_edges_face is None:
+                # We seem to have found the edge of a mesh. Continue from first ixp in
+                # in opposite direction unless we already did once
+                if contour_is_open:
+                    # We already found one end of this open contour.
+                    # Now we've found the other so we're done
+                    break
+                next_ixp = first_ixp
+                contour.reverse()
+                prev_face = first_ixp.l_face
+                ixp = next_ixp
+                ix_points.remove(ixp)
+                contour_is_open = True
+                continue
+                
+            for cand_edge in this_edges_face.edges:
+                if cand_edge is ixp.edge:
+                    continue
+                # if this candidate-edge crosses this edge's face, it's x-point is the next point
+                if (cand_edge, ixp.face) in self._saved_results:
+                    next_ixp = self._saved_results[(cand_edge, ixp.face)]
+                    prev_face = this_edges_face
+                    break
+            
+            if next_ixp is None:
+                # We didn't find it so the next xpoint must be on other face
+                for cand_edge in ixp.face.edges:
+                    if (cand_edge, this_edges_face) in self._saved_results:
+                        next_ixp = self._saved_results[(cand_edge, this_edges_face)]
+                        prev_face = ixp.face
+                        break
+
+            ixp = next_ixp
+            ix_points.remove(ixp)
+            contour.append(ixp)
+
+        return contour
+        
 
     def _get_norm_squared(self, vector):
         return (vector[0]*vector[0] +
